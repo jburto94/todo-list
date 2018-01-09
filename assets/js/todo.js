a17c4854036f68d1761006d9ac4ace97cd3b09fb
jburto94 <jburto94@yahoo.com>
2018-01-09 14:48:56 -0500

    Added basic item completion, deletion, and addition

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,2 +1,22 @@
+// Mark todo as done by clicking
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('completed');
+});
 
+// Click X to remove todo
+$('ul').on('click', 'span', function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	// Stops marking as done when clicking on remove
+	event.stopPropagation();
+});
 
+$('input[type="text"]').keypress(function(event) {
+	if(event.which === 13) {
+		// Add new todo item when the user presses enter
+		var newTodo = $(this).val();
+		$(this).val('');
+		$('ul').append('<li><span>X</span> ' +  newTodo + '</li>');
+	}
+});
